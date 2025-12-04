2d5fd61736133bad56d31b2e6ed918351d1c8718
Samuel <sfoletto@gmail.com>
2025-12-03 21:01:40 -0300

    Correção no construtor

--- src/ConsultaViaCep.php
+++ src/ConsultaViaCep.php
@@ -6,14 +6,15 @@ use GuzzleHttp\Client;
 class ConsultaViaCep
 {
     const BASE_ENDPOINT = 'https://viacep.com.br/ws/';
-    public function __construct(Cep $cep){
-        $this->cep = $cep;
+    private $client;
+    public function __construct(Client $client){
+        $this->client = $client;
     }
 
     public function consultaCep($cep){
         $url = self::BASE_ENDPOINT . "{$cep}/json";
 
-        $response = $this->cep->request('GET', $url);
+        $response = $this->client->request('GET', $url);
 
         return json_decode($response->getBody()->getContents(), true);
 
